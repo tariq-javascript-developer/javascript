95a223a22190b8eb7d722782d101c6f4d3056772
tariq-javascript-developer <tariq.students.kicsit@gmail.com>
2020-08-19 13:29:33 +0500

    commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 
-function ageInDays(){
+function myFunction(){
     var birthYear =  prompt('What year were you born Good Friend?');
-    var ageindayss = (2020 -birthYear)*365;
-    console.log(ageindayss);
+    var ageInDayss = (2020 - birthYear)*365;
+    console.log(ageInDayss);
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('Your are'+ ageInDayss +' days');
+    console.log(textAnswer);
+    h1.setAttribute('id','ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').innerHTML(h1);
 }
