239fac37336daad0dae2888d35d03684b276b7b9
tariq-javascript-developer <tariq.students.kicsit@gmail.com>
2020-08-19 14:00:32 +0500

    commit

--- js/script.js
+++ js/script.js
@@ -2,11 +2,14 @@
 function myFunction(){
     var birthYear =  prompt('What year were you born Good Friend?');
     var ageInDayss = (2020 - birthYear)*365;
-    console.log(ageInDayss);
     var h1 = document.createElement('h1');
-    var textAnswer = document.createTextNode('Your are'+ ageInDayss +' days');
-    console.log(textAnswer);
+    var textNode = document.createTextNode('Your are '+ ageInDayss +' days');
     h1.setAttribute('id','ageInDays');
-    h1.appendChild(textAnswer);
-    document.getElementById('flex-box-result').innerHTML(h1);
+    h1.appendChild(textNode);
+    document.getElementById("myDIV").appendChild(h1);
+    
+}
+
+function reset(){
+    document.getElementById('ageInDays').remove();
 }
